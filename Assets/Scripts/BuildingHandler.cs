67ce49aea4a0ca92ca11f1d5a57213fabb23c9fc
Yixi <lyh1112@gmail.com>
2020-01-31 18:32:25 +0800

    base place the building

---
+++ Assets/Scripts/BuildingHandler.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingHandler : MonoBehaviour
+{
+    private Building _selectBuilding;
+    private BuildingManager _buildingManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _buildingManager = GetComponent<BuildingManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && _selectBuilding)
+        {
+            InteractWithGround();
+        }
+    }
+
+    public void EnableBuilder(Building building)
+    {
+        _selectBuilding = building;
+    }
+
+    void InteractWithGround()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            var gridPosition = _buildingManager.CalculateGridPosition(hit.point);
+            if (!UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject() &&
+                !_buildingManager.IsHaveBuilding(gridPosition))
+            {
+                _buildingManager.addBuilding(_selectBuilding, gridPosition);
+            }
+        }
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,13 +7,11 @@ public class GameManager : MonoBehaviour
 {
     public int mapWidth = 1;
     public int mapHeight = 1;
-   
+
     // Start is called before the first frame update
     void Start()
     {
     }
 
     // Update is called once per frame
-
-    
 }
